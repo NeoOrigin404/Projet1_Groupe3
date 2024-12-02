13e621185cd546c5ad59f91a4b66e85f3481e804
Julien PICART <julienpicart01@gmail.com>
2024-12-02 08:46:28 +0100

    Ajout du projet complet

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+console.log("scrpit chargeé");
+
 // Tableau des stories
 const tableauStories = [
   {
@@ -73,7 +75,7 @@ function createdCategory(createCategory) {
   // Création de boucle Each pour générer les catégories
   createCategory.forEach((divCategory) => {
     // Création de la div category
-    const category = document.createElement("div");
+    const category = document.createElement("button");
     category.classList.add(".category");
     categories.appendChild(category);
 
@@ -87,6 +89,87 @@ function createdCategory(createCategory) {
 
 // Execution de la fonction
 createdCategory(tableauCategories);
+
+// test interactivité icones du menu
+// script.js
+const icons = document.querySelectorAll(".bottom-menu .icon");
+
+icons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    icons.forEach((i) => i.classList.remove("active"));
+    icon.classList.add("active");
+  });
+});
+
+// CAROUSEL
+
+//Variables
+const track = document.querySelector(".carousel-track");
+const items = Array.from(track.children);
+const pagination = document.querySelector(".carousel-pagination");
+
+//Calcul de la largeur des items
+const itemWidth = items[0].getBoundingClientRect().width;
+
+//Positionner les items en ligne
+items.forEach((item, index) => {
+  item.style.left = `${itemWidth * index}px`;
+});
+let currentIndex = 0;
+
+//Fonction pour déplacer le carousel
+const moveToItem = (index) => {
+  track.style.transform = `translateX(-${itemWidth * index}px)`;
+  currentIndex = index;
+  updatePagination();
+};
+
+// Générer points de pagination
+
+items.forEach((_, index) => {
+  const dot = document.createElement("div");
+  dot.classList.add("dot");
+  if (index === 0) dot.classList.add("active");
+  dot.addEventListener("click", () => moveToItem(index));
+  pagination.appendChild(dot);
+});
+
+//Mettre à jour les points de pagination
+
+const updatePagination = () => {
+  const dots = document.querySelectorAll(".carousel-pagination .dot");
+  dots.forEach((dot, index) => {
+    if (index === currentIndex) {
+      dot.classList.add("active");
+    } else {
+      dot.classList.remove("active");
+    }
+  });
+};
+
+// SWIPE SUR MOBILE
+let startX = 0;
+track.addEventListener("touchstart", (e) => {
+  startX = e.touches[0].clientX;
+});
+track.addEventListener("touchend", (e) => {
+  const endX = e.changedTouches[0].clientX;
+  if (startX - endX > 50 && currentIndex < items.length - 1) {
+    moveToItem(currentIndex + 1); // swipe gauche = suivant
+  } else if (endX - startX > 50 && currentIndex > 0) {
+    moveToItem(currentIndex - 1); //Swipe droite = precedent
+  }
+});
+
+// dynamisme catégories
+const categories = Array.from(document.getElementsByTagName("h2"));
+
+categories.forEach((h2) => {
+  h2.addEventListener("click", () => {
+    categories.forEach((i) => i.classList.remove("active"));
+    h2.classList.add("active");
+  });
+});
 
 // Tableau des posts
 const posts = [
