7af45c478d16c9c624c172c6381599fac5152d2f
Julien PICART <julienpicart01@gmail.com>
2024-12-02 12:32:45 +0100

    Avancement de la pop-up

--- script.js
+++ script.js
@@ -258,3 +258,11 @@ function initializeCarousel() {
 setTimeout(() => {
   initializeCarousel();
 }, 100);
+
+// Pop-up (modal)
+const dialog = document.querySelector("dialog");
+const openModalButton = document.querySelector(".open_modal");
+const closeModalButton = document.querySelector(".close_modal");
+
+openModalButton.addEventListener("click", () => dialog.showModal());
+closeModalButton.addEventListener("click", () => dialog.close());
