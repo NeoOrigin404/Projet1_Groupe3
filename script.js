d79c7e6496f6330ec8a7dfa0b9d9b15f12915f9f
Julien PICART <julienpicart01@gmail.com>
2024-12-03 21:58:58 +0100

    Ajout de l'animation du like sur la partie pop-up : OK

--- script.js
+++ script.js
@@ -282,3 +282,20 @@ darkButton.addEventListener("click", () => {
   body.classList.remove("lightmode");
   body.classList.add("darkmode");
 });
+
+// Like button
+const heartIcon = document.querySelector(".heart-icon");
+const likesAmountLabel = document.querySelector(".likes-amount");
+
+let likesAmount = 7;
+
+heartIcon.addEventListener("click", () => {
+  heartIcon.classList.toggle("liked");
+  if (heartIcon.classList.contains("liked")) {
+    likesAmount++;
+  } else {
+    likesAmount--;
+  }
+
+  likesAmountLabel.innerHTML = likesAmount;
+});
